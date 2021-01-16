bded87e5b377ebbdd4e9bb4bb2432d0c39daf38c
Inderjit Gill <email@indy.io>
2021-01-16 23:24:12 +0000

    not launching emacs and brave at startup

--- config.h
+++ config.h
@@ -37,8 +37,8 @@ static const char *colors[][3]      = {
 static const char *const autostart[] = {
   "sh", "-c", "while :; do feh --randomize --bg-max /home/indy/gdrive/art/* -; sleep 30m; done", NULL,
 	"dwmblocks", NULL,
-	"emacs", NULL,
-	"brave-browser", NULL,
+  // "emacs", NULL,
+  // "brave-browser", NULL,
 	NULL /* terminate */
 };
 
@@ -52,10 +52,10 @@ static const Rule rules[] = {
 	 */
 	/* class               instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",              NULL,       NULL,       0,            1,           -1 },
-	{ "Emacs",             NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Brave-browser",     NULL,       NULL,       1 << 0,       0,           -1 },
-	{ "Chromium-browser",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Firefox",           NULL,       NULL,       1 << 8,       0,           -1 },
+	// { "Emacs",             NULL,       NULL,       1 << 1,       0,           -1 },
+	// { "Brave-browser",     NULL,       NULL,       1 << 0,       0,           -1 },
+	// { "Chromium-browser",  NULL,       NULL,       1 << 8,       0,           -1 },
+	// { "Firefox",           NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */

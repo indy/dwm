f84ff460e2ec1c36be4622d083cb52c86487bdd4
Inderjit Gill <email@indy.io>
2022-05-02 15:57:55 +0100

    replaced tabs with spaces

--- config.h
+++ config.h
@@ -26,28 +26,28 @@ static const char *colors[][3]      = {
 static const char *const autostart[] = {
   "sh", "-c", "while :; do feh --randomize --bg-max /home/indy/gdrive/art/* -; sleep 30m; done", NULL,
   "dwm-bar", NULL,
-	NULL /* terminate */
+  NULL /* terminate */
 };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const char ptagf[] = "[%s %s]";	/* format of a tag label */
-static const char etagf[] = "[%s]";	/* format of an empty tag */
-static const int lcaselbl = 0;		/* 1 means make tag label lowercase */
+static const char ptagf[] = "[%s %s]";  /* format of a tag label */
+static const char etagf[] = "[%s]";  /* format of an empty tag */
+static const int lcaselbl = 0;    /* 1 means make tag label lowercase */
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class               instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",              NULL,       NULL,       0,            1,           -1 },
+  /* xprop(1):
+   *  WM_CLASS(STRING) = instance, class
+   *  WM_NAME(STRING) = title
+   */
+  /* class               instance    title       tags mask     isfloating   monitor */
+  { "Gimp",              NULL,       NULL,       0,            1,           -1 },
 #if 0
-	{ "Emacs",             NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Brave-browser",     NULL,       NULL,       1 << 0,       0,           -1 },
-	{ "Chromium-browser",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Firefox",           NULL,       NULL,       1 << 8,       0,           -1 },
+  { "Emacs",             NULL,       NULL,       1 << 1,       0,           -1 },
+  { "Brave-browser",     NULL,       NULL,       1 << 0,       0,           -1 },
+  { "Chromium-browser",  NULL,       NULL,       1 << 8,       0,           -1 },
+  { "Firefox",           NULL,       NULL,       1 << 8,       0,           -1 },
 #endif
 };
 
@@ -57,21 +57,21 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
-	/* symbol     arrange function */
-	{ "|M|",      centeredmaster }, /* first entry is default */
-	{ "=[]",      tile },
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "(M)",      monocle },
+  /* symbol     arrange function */
+  { "|M|",      centeredmaster }, /* first entry is default */
+  { "=[]",      tile },
+  { "><>",      NULL },    /* no layout function means floating behavior */
+  { "(M)",      monocle },
 };
 
 /* key definitions */
 #define MODKEY Mod4Mask
 #define ALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+  { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+  { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+  { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+  { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -92,33 +92,33 @@ static const char *audioprevcmd[]   = { "dwm-audio", "prev", NULL };
 static const char *audionextcmd[]   = { "dwm-audio", "next", NULL };
 
 static Key keys[] = {
-	/* modifier           key                      function        argument */
-	{ MODKEY,             XK_d,                    spawn,          {.v = commondmenucmd } },
-	{ MODKEY|ShiftMask,   XK_d,                    spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,   XK_Return,               spawn,          {.v = termcmd } },
+  /* modifier           key                      function        argument */
+  { MODKEY,             XK_d,                    spawn,          {.v = commondmenucmd } },
+  { MODKEY|ShiftMask,   XK_d,                    spawn,          {.v = dmenucmd } },
+  { MODKEY|ShiftMask,   XK_Return,               spawn,          {.v = termcmd } },
   { MODKEY,             XK_grave,                togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,             XK_space,                togglebar,      {0} }, // the obvious key for togglebar
-	{ MODKEY,             XK_j,                    focusstack,     {.i = +1 } },
-	{ MODKEY,             XK_k,                    focusstack,     {.i = -1 } },
-	{ ALTKEY,             XK_0,                    focusstack,     {.i = +1 } }, // emacs finger memory
+  { MODKEY,             XK_space,                togglebar,      {0} }, // the obvious key for togglebar
+  { MODKEY,             XK_j,                    focusstack,     {.i = +1 } },
+  { MODKEY,             XK_k,                    focusstack,     {.i = -1 } },
+  { ALTKEY,             XK_0,                    focusstack,     {.i = +1 } }, // emacs finger memory
 #if 0
-	{ MODKEY,             XK_i,                    incnmaster,     {.i = +1 } },
-	{ MODKEY,             XK_d,                    incnmaster,     {.i = -1 } },
+  { MODKEY,             XK_i,                    incnmaster,     {.i = +1 } },
+  { MODKEY,             XK_d,                    incnmaster,     {.i = -1 } },
 #endif
-	{ MODKEY,             XK_h,                    setmfact,       {.f = -0.05} },
-	{ MODKEY,             XK_l,                    setmfact,       {.f = +0.05} },
-	{ MODKEY,             XK_Return,               zoom,           {0} },
-	{ ALTKEY,             XK_Return,               zoom,           {0} }, // emacs typo prevention
+  { MODKEY,             XK_h,                    setmfact,       {.f = -0.05} },
+  { MODKEY,             XK_l,                    setmfact,       {.f = +0.05} },
+  { MODKEY,             XK_Return,               zoom,           {0} },
+  { ALTKEY,             XK_Return,               zoom,           {0} }, // emacs typo prevention
 #if 0
-	{ MODKEY,             XK_Tab,                  view,           {0} },
+  { MODKEY,             XK_Tab,                  view,           {0} },
 #endif
-	{ MODKEY|ShiftMask,   XK_q,                    killclient,     {0} },
-	{ MODKEY,             XK_f, /* fullscreen */   togglefullscreen, {0} },
+  { MODKEY|ShiftMask,   XK_q,                    killclient,     {0} },
+  { MODKEY,             XK_f, /* fullscreen */   togglefullscreen, {0} },
 
-	{ MODKEY,             XK_u, /* centered */     setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,             XK_t, /* tile     */     setlayout,      {.v = &layouts[1]} },
-	{ MODKEY|ShiftMask,   XK_f, /* float    */     setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,             XK_s, /* single   */     setlayout,      {.v = &layouts[3]} },
+  { MODKEY,             XK_u, /* centered */     setlayout,      {.v = &layouts[0]} },
+  { MODKEY,             XK_t, /* tile     */     setlayout,      {.v = &layouts[1]} },
+  { MODKEY|ShiftMask,   XK_f, /* float    */     setlayout,      {.v = &layouts[2]} },
+  { MODKEY,             XK_s, /* single   */     setlayout,      {.v = &layouts[3]} },
   // audio controls
   { MODKEY,             XK_v,                    spawn,          {.v = audioplaycmd } },
   { MODKEY,             XK_b,                    spawn,          {.v = audioprevcmd } },
@@ -134,39 +134,39 @@ static Key keys[] = {
   { 0,                  XF86XK_AudioLowerVolume, spawn,          {.v = audiodeccmd } },
   { 0,                  XF86XK_AudioRaiseVolume, spawn,          {.v = audioinccmd } },
 #if 0
-	{ MODKEY,             XK_space,                setlayout,      {0} }, // key taken by togglebar
+  { MODKEY,             XK_space,                setlayout,      {0} }, // key taken by togglebar
 #endif
-	{ MODKEY|ShiftMask,   XK_space,                togglefloating, {0} },
+  { MODKEY|ShiftMask,   XK_space,                togglefloating, {0} },
 #if 0
-	{ MODKEY,             XK_0,                    view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,   XK_0,                    tag,            {.ui = ~0 } },
-	{ MODKEY|ShiftMask,   XK_comma,                tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,   XK_period,               tagmon,         {.i = +1 } },
+  { MODKEY,             XK_0,                    view,           {.ui = ~0 } },
+  { MODKEY|ShiftMask,   XK_0,                    tag,            {.ui = ~0 } },
+  { MODKEY|ShiftMask,   XK_comma,                tagmon,         {.i = -1 } },
+  { MODKEY|ShiftMask,   XK_period,               tagmon,         {.i = +1 } },
 #endif
-	TAGKEYS(              XK_1,                                    0)
-	TAGKEYS(              XK_2,                                    1)
-	TAGKEYS(              XK_3,                                    2)
-	TAGKEYS(              XK_4,                                    3)
-	TAGKEYS(              XK_5,                                    4)
-	TAGKEYS(              XK_6,                                    5)
-	TAGKEYS(              XK_7,                                    6)
-	TAGKEYS(              XK_8,                                    7)
-	TAGKEYS(              XK_9,                                    8)
-	{ MODKEY|ShiftMask,   XK_e, /* exit dwm */     quit,           {0} },
+  TAGKEYS(              XK_1,                                    0)
+  TAGKEYS(              XK_2,                                    1)
+  TAGKEYS(              XK_3,                                    2)
+  TAGKEYS(              XK_4,                                    3)
+  TAGKEYS(              XK_5,                                    4)
+  TAGKEYS(              XK_6,                                    5)
+  TAGKEYS(              XK_7,                                    6)
+  TAGKEYS(              XK_8,                                    7)
+  TAGKEYS(              XK_9,                                    8)
+  { MODKEY|ShiftMask,   XK_e, /* exit dwm */     quit,           {0} },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+  /* click                event mask      button          function        argument */
+  { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+  { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+  { ClkWinTitle,          0,              Button2,        zoom,           {0} },
+  { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+  { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+  { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+  { ClkTagBar,            0,              Button1,        view,           {0} },
+  { ClkTagBar,            0,              Button3,        toggleview,     {0} },
+  { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+  { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
